34676aa014f793ffbc8eb3f815824ab9f32f1b79
CoronelChris <yafte1990@gmail.com>
2025-08-13 16:39:56 +0100

    Primer commit

---
+++ src/main/java/com/aluracursos/gutenberg_catalogue/GutenbergCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.gutenberg_catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutenbergCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutenbergCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/gutenberg_catalogue/GutenbergCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.gutenberg_catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutenbergCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
